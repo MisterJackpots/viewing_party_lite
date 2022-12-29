199b4ab789a197135c328b6f1c2c5a1aee4bd403
Lucas Colwell <colwellc37@gmail.com>
2022-12-28 22:42:16 -0700

    Story 1

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'The User registration' do
   describe 'As a User' do
     describe 'When I visit the User Registration Page' do
       before(:each) do
-        @user1 = User.create!(name: 'Mary', email: 'mary@gmail.com')
-        @user2 = User.create!(name: 'Larry', email: 'larry@hotmail.com')
-        @user3 = User.create!(name: 'Sherri', email: 'sherri@aol.com')
+        @user1 = User.create!(name: 'Mary', email: 'mary@gmail.com', password: 'password123', password_confirmation: 'password123')
+        @user2 = User.create!(name: 'Larry', email: 'larry@hotmail.com', password: 'password123', password_confirmation: 'password123')
+        @user3 = User.create!(name: 'Sherri', email: 'sherri@aol.com', password: 'password123', password_confirmation: 'password123')
       end
       it 'I see a link to create a new user' do
         visit root_path
@@ -14,7 +14,9 @@ RSpec.describe 'The User registration' do
         expect(current_path).to eq(register_path)
 
         fill_in 'Name', with: 'Martin'
-        fill_in 'Email', with: 'martin@gmail.com'
+        fill_in 'Email', with: 'martin31@gmail.com'
+        fill_in 'Password', with: 'password123'
+        fill_in 'Password confirmation', with: 'password123'
         click_on 'Create Account'
 
         expect(page).to have_content("Martin")
@@ -30,6 +32,7 @@ RSpec.describe 'The User registration' do
 
         fill_in 'Name', with: "Mary"
         fill_in 'Email', with: "Mary@gmail.com"
+        fill_in 'Password', with: 'password123'
         click_on 'Create Account'
 
         expect(current_path).to eq(register_path)
